2f675c18c13290f4ef4836f97c208e590963f7fc
ratminen <ghgpoklonik16@gmail.com>
2025-12-28 15:30:16 +0700

    8. Edit PhotoGallery

--- app/src/main/java/com/example/photogallery/MainActivity.kt
+++ app/src/main/java/com/example/photogallery/MainActivity.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Search
@@ -26,6 +31,7 @@ import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -38,15 +44,17 @@ import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.example.photogallery.flickr.Photo
 import com.example.photogallery.ui.theme.PhotoGalleryTheme
+import kotlin.getValue
 
 class MainActivity : ComponentActivity() {
+    private val viewModel by viewModels<PhotoViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             PhotoGalleryTheme {
                 Scaffold(modifier = Modifier.fillMaxSize().background(color = Color.Gray)) { innerPadding ->
-                    PhotoGalleryScreen(modifier = Modifier.padding(innerPadding))
+                    PhotoGalleryScreen(viewModel,modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -54,100 +62,118 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun PhotoGalleryScreen(modifier: Modifier){
-
-}
-@Composable
-fun PhotoItem(photo: Photo) {
-    Card(
-        modifier = Modifier
-            .padding(4.dp)
-            .fillMaxWidth()
-    ) {
-        AsyncImage(
-            model = photo.url,
-            contentDescription = photo.title,
-            modifier = Modifier
-                .aspectRatio(1f)
-        )
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun PhotoGalleryTopBar(
-    onSearch: (String) -> Unit,
-    onStartPolling: () -> Unit,
-    onMenuAction1: () -> Unit,
-    onMenuAction2: () -> Unit
-) {
-    var expanded by remember { mutableStateOf(false) }
-    var isSearching by remember { mutableStateOf(false) }
-    var searchText by remember { mutableStateOf("") }
-    TopAppBar(title = {
-        if (isSearching) {
-            TextField(
-                value = searchText,
-                onValueChange = { searchText = it },
-                placeholder = { Text("Search photos") },
-                singleLine = true,
-                modifier = Modifier.fillMaxWidth()
+fun PhotoGalleryScreen(viewModel: PhotoViewModel, modifier: Modifier) {
+        val photos by viewModel.photos.collectAsState()
+        Column(modifier= Modifier){
+            PhotoGalleryTopBar(
+                onSearch = { query -> println("Searching: $query") },
+                onStartPolling = { println("Searching:onStartPolling ") },
+                onMenuAction1 = { println("Searching:onStartPolling ") },
+                onMenuAction2 = { println("Searching:onStartPolling ") }
             )
-        } else {
-            Text("PhotoGallery", fontSize = 20.sp, color = Color.White)
+            LazyVerticalGrid(
+                columns = GridCells.Adaptive(120.dp),
+                modifier = Modifier.fillMaxSize()
+            ) {
+                items(photos) { photo ->
+                    PhotoItem(photo)
+                }
+            }
         }
-    }, actions = {
-        if (!isSearching) { // Кнопка поиска
-            IconButton(onClick = { isSearching = true }) {
-                Icon(
-                    Icons.Default.Search,
-                    contentDescription = "Search",
-                    tint = Color.White
+    }
+
+    @Composable
+    fun PhotoItem(photo: Photo) {
+        Card(
+            modifier = Modifier
+                .padding(4.dp)
+                .fillMaxWidth()
+        ) {
+            AsyncImage(
+                model = photo.url,
+                contentDescription = photo.title,
+                modifier = Modifier
+                    .aspectRatio(1f)
+            )
+        }
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun PhotoGalleryTopBar(
+        onSearch: (String) -> Unit,
+        onStartPolling: () -> Unit,
+        onMenuAction1: () -> Unit,
+        onMenuAction2: () -> Unit
+    ) {
+        var expanded by remember { mutableStateOf(false) }
+        var isSearching by remember { mutableStateOf(false) }
+        var searchText by remember { mutableStateOf("") }
+        TopAppBar(title = {
+            if (isSearching) {
+                TextField(
+                    value = searchText,
+                    onValueChange = { searchText = it },
+                    placeholder = { Text("Search photos") },
+                    singleLine = true,
+                    modifier = Modifier.fillMaxWidth()
                 )
+            } else {
+                Text("PhotoGallery", fontSize = 20.sp, color = Color.White)
             }
-            TextButton(onClick = onStartPolling) {
-                Text(
-                    "START POLLING",
-                    color = Color.White
-                )
-            }
-            Box {
-                IconButton(onClick = { expanded = true }) {
+        }, actions = {
+            if (!isSearching) { // Кнопка поиска
+                IconButton(onClick = { isSearching = true }) {
                     Icon(
-                        Icons.Default.MoreVert,
-                        contentDescription = "Menu",
+                        Icons.Default.Search,
+                        contentDescription = "Search",
                         tint = Color.White
                     )
                 }
-                DropdownMenu(
-                    expanded = expanded,
-                    onDismissRequest = { expanded = false }
-                ) {
-                    DropdownMenuItem(
-                        text = { Text("Action 1") },
-                        onClick = {
-                            onMenuAction1()
-                            expanded = false
-                        })
-                    DropdownMenuItem(
-                        text =
-                            { Text("Action 2") },
-                        onClick = {
-                            onMenuAction2()
-                            expanded = false
-                        })
+                TextButton(onClick = onStartPolling) {
+                    Text(
+                        "START POLLING",
+                        color = Color.White
+                    )
+                }
+                Box {
+                    IconButton(onClick = { expanded = true }) {
+                        Icon(
+                            Icons.Default.MoreVert,
+                            contentDescription = "Menu",
+                            tint = Color.White
+                        )
+                    }
+                    DropdownMenu(
+                        expanded = expanded,
+                        onDismissRequest = { expanded = false }
+                    ) {
+                        DropdownMenuItem(
+                            text = { Text("Action 1") },
+                            onClick = {
+                                onMenuAction1()
+                                expanded = false
+                            })
+                        DropdownMenuItem(
+                            text =
+                                { Text("Action 2") },
+                            onClick = {
+                                onMenuAction2()
+                                expanded = false
+                            })
+                    }
+                }
+            } else {
+                TextButton(onClick = {
+                    onSearch(searchText)
+                    searchText = ""
+                    isSearching = false
+                }) {
+                    Text(
+                        "Done",
+                        color = Color.White
+                    )
                 }
             }
-        } else {
-            TextButton(onClick = {
-                onSearch(searchText)
-                searchText = ""
-                isSearching = false }) {
-                Text(
-                    "Done",
-                    color = Color.White
-                )
-            }
-        }
-    }, colors = TopAppBarDefaults.topAppBarColors(containerColor = Color(0xFF6200EE)))
-}
+        }, colors = TopAppBarDefaults.topAppBarColors(containerColor = Color(0xFF6200EE)))
+    }
