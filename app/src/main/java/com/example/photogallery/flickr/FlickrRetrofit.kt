0978cf3b39269c28fc16c2043a571fd49dac2977
ratminen <ghgpoklonik16@gmail.com>
2025-12-28 15:06:26 +0700

    6. Add FlickrRetrofit

---
+++ app/src/main/java/com/example/photogallery/flickr/FlickrRetrofit.kt
@@ -0,0 +1,31 @@
+package com.example.photogallery.flickr
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+object FlickrRetrofit {
+    private const val BASE_URL = "https://www.flickr.com/"
+
+    private val logger = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(logger)
+        .build()
+
+    private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
+    val api: FlickrApi = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .client(client)
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .build()
+        .create(FlickrApi::class.java)
+}
