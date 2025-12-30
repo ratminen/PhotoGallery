1341250dcc026e646b04b90fe6259c66741f6c4f
ratminen <ghgpoklonik16@gmail.com>
2025-12-30 13:57:29 +0700

    15. Add Favorite method's

--- app/src/main/java/com/example/photogallery/MainActivity.kt
+++ app/src/main/java/com/example/photogallery/MainActivity.kt
@@ -8,15 +8,17 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Search
@@ -26,6 +28,7 @@ import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -33,14 +36,12 @@ import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
@@ -100,6 +101,47 @@ fun PhotoGalleryScreen(viewModel: PhotoViewModel, padding: PaddingValues) {
             )
         }
     }
+
+@Composable
+fun FavoriteTitleRow(head1: String, head2: String) {
+    Row (
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.primary)
+            .fillMaxWidth()
+            .padding(5.dp)
+    ) {
+        Text(head1, color = Color.White, modifier = Modifier.weight(0.3f))
+        Text(head2, color = Color.White, modifier = Modifier.weight(0.7f))
+    }
+}
+@Composable
+fun FavoritePhotoRow(title: String, url: String) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(5.dp)
+    ) {
+        Text(title, modifier = Modifier.weight(0.3f))
+        Text(url, modifier = Modifier.weight(0.7f))
+    }
+}
+@Composable
+fun FavoritesScreen(viewModel: PhotoViewModel, padding: PaddingValues) {
+    val favorites by viewModel.allFavorites.observeAsState(emptyList())
+
+    LazyColumn (
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(padding)
+    ) {
+        item {
+            FavoriteTitleRow(head1 = "Title", head2 = "URL")
+        }
+        items(favorites) { photo ->
+            FavoritePhotoRow(title = photo.title, url = photo.url)
+        }
+    }
+}
 
     @OptIn(ExperimentalMaterial3Api::class)
     @Composable
